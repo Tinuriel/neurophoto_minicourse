efd119d5cdb26433edc1da2c3ea3dde34a920111
Tinuriel <tinuriel@gmail.com>
2026-06-30 12:47:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -188,6 +188,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "scenario",
     "old-vs-new",
     "benefits",
+    "transformation",
     "gallery",
     "audience",
     "author",
